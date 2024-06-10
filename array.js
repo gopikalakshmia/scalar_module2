1ef83089d6323e3caf57da6d411e016affde9a1c
gopika <gopikalakshmia@gmail.com>
2024-06-10 13:48:57 -0700

    first

--- array.js
+++ array.js
@@ -1,3 +1,24 @@
 var arr=[1,"hello",true];
 console.log(arr);
+//Array length
 console.log(arr.length);
+
+
+//Pop Array-Remove the last value of array
+var a=[1,2,3,4];
+var b=a.pop();
+console.log("pop value ",b);
+console.log("popped Array ",a);
+
+//push-add new element to the end of array
+a.push(5)
+console.log("Push array ",a);
+
+//Shift-will remove the first element of array
+a.shift();
+console.log("Shift Array ",a);
+
+//Unshift array will added new element to the start of an Array
+
+a.unshift(1);
+console.log("Unshift array ",a);
